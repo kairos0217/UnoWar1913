ff9638e78c1078c7fb6272654cb1c3fe897b7c55
kairos0217 <151873721+kairos0217@users.noreply.github.com>
2024-11-05 12:54:47 -0600

    Finalized AIs: -Smallest AI -Biggest AI -Random AI
    passed AI tests

--- src/student/AI.java
+++ src/student/AI.java
@@ -1,4 +1,16 @@
 package student;
 
 public class AI {
+
+    public Card getPlay(Hand hand, CardPile cardPile){
+        for(int i = 0; i < hand.getSize(); i++){
+            if(cardPile.canPlay(hand.get(i))) return hand.get(i);
+        }
+        return null;
+    }
+
+    @Override
+    public String toString(){
+        return "Random Card AI";
+    }
 }
--- src/student/SmallestCardAI.java
+++ src/student/SmallestCardAI.java
@@ -1,4 +1,22 @@
 package student;
 
 public class SmallestCardAI extends AI{
+    @Override
+    public Card getPlay(Hand hand, CardPile cardPile) {
+        Card returned = null;
+        for(int i = 0; i < hand.getSize(); i++){
+            Card cur = hand.get(i);
+            if(cardPile.canPlay(cur)){
+                if(returned == null || cur.getRankNum() < returned.getRankNum()){
+                    returned = cur;
+                }
+            }
+        }
+        return returned;
+    }
+
+    @Override
+    public String toString(){
+        return "Smallest Card AI";
+    }
 }
